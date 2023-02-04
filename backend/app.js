23005e07f17e3a31f5e44771dab258c69b26f320
starvestt <starvestt@gmail.com>
2023-02-04 19:35:40 +0530

    refactor: separated app and server for tests

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const errorHandler = require("./middleware/errorMiddleware");
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+app.use("/api/blogs", require("./routes/blogRoutes"))
+
+app.use(errorHandler)
+
+module.exports = app
